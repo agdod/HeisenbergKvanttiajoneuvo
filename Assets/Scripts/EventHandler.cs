689e9d7ba29dba3e8b43805e6f3f10ad280fc84d
agdodson <andy.dodson@outlook.com>
2021-10-05 16:50:49 +0300

    - Added new C# events

--- Assets/Scripts/EventHandler.cs
+++ Assets/Scripts/EventHandler.cs
@@ -7,9 +7,14 @@ public class EventHandler : MonoBehaviour
 	public delegate void GameOverReason(string message);
 	public delegate void GameOverAction();
 
+	public delegate void CollectUncertainityValues();
+	public delegate void TurnAction();
+
 	public static event GameOverReason gameOverReason;
 	public static event GameOverAction onGameOver;
-
+	public static event CollectUncertainityValues onCollectUncertainityValues;
+	public static event TurnAction onStartTurn;
+	public static event TurnAction onEndTurn;
 
 
 	public void OnGameOverReason(string message)
@@ -27,6 +32,30 @@ public class EventHandler : MonoBehaviour
 		if (onGameOver != null)
 		{
 			onGameOver.Invoke();
+		}
+	}
+
+	public void OnCollectUncertainityValues()
+	{
+		if (onCollectUncertainityValues != null)
+		{
+			onCollectUncertainityValues.Invoke();
+		}
+	}
+
+	public void OnStartTurn()
+	{
+		if (onStartTurn != null)
+		{
+			onStartTurn.Invoke();
+		}
+	}
+
+	public void OnEndTurn()
+	{
+		if (onEndTurn != null)
+		{
+			onEndTurn.Invoke();
 		}
 	}
 }
