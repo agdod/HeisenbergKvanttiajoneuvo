fe29e840e8fa3340b05174a5f0ff124d3a9ee791
agdodson <andy.dodson@outlook.com>
2021-10-05 16:51:35 +0300

    - Control of the dirrection/velocity inputs and UI elements.

--- Assets/Scripts/UncertainityPanel.cs
+++ Assets/Scripts/UncertainityPanel.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum Uncertainity { velocity, direction }
+
 public class UncertainityPanel : MonoBehaviour
 {
-	public enum Uncertainity { velocity, direction }
-
+	[Header("Identifier")]
+	[SerializeField] private Uncertainity uncertainity;
+	[Space]
 	[SerializeField] private Toggle thisToggle;
 	[SerializeField] private Slider thisSlider;
 	[SerializeField] private TMPro.TMP_Text thisValue;
-	[SerializeField] private Uncertainity uncertainity;
+	[SerializeField] private floatVarible uncertaintyVariable;
+
+	private void Awake()
+	{
+		EventHandler.onCollectUncertainityValues += GenerateThisValue;
+		EventHandler.onStartTurn += DisableAll;
+		EventHandler.onEndTurn += EnableAndReset;
+	}
 
 	private void Start()
 	{
@@ -22,10 +33,13 @@ public class UncertainityPanel : MonoBehaviour
 		if (thisToggle.isOn)
 		{
 			thisSlider.gameObject.SetActive(true);
+			thisValue.text = thisSlider.value.ToString();
 		}
 		else
 		{
 			thisSlider.gameObject.SetActive(false);
+			thisValue.text = "###";
+
 		}
 	}
 
@@ -34,4 +48,43 @@ public class UncertainityPanel : MonoBehaviour
 		thisValue.text = thisSlider.value.ToString();
 	}
 
+	private void GenerateThisValue()
+	{
+		if (thisToggle.isOn)
+		{
+			// thisSlider.value
+			uncertaintyVariable.value = thisSlider.value;
+		}
+		else
+		{
+			thisSlider.value = 0;
+			/*thisSlider.gameObject.SetActive(false);
+			thisValue.text = "###";*/
+			// Generate random value
+			GenerateRandomValue();
+		}
+	}
+
+	private void GenerateRandomValue()
+	{
+		// Random seed value
+		int initalSeed = (int)DateTime.Now.Ticks;
+		UnityEngine.Random.InitState(initalSeed);
+		// random value, between min and max of slider values.
+		uncertaintyVariable.value = UnityEngine.Random.Range(thisSlider.minValue, thisSlider.maxValue);
+	}
+
+	private void DisableAll()
+	{
+		thisSlider.interactable = false;
+		thisToggle.interactable = false;
+	}
+
+	private void EnableAndReset()
+	{
+		thisSlider.interactable = true;
+		thisSlider.value = 0;
+		AdjustThisValue();
+		thisToggle.interactable = true;
+	}
 }
